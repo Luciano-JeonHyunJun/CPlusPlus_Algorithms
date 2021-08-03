a2f21b25cf442e8a983648b4ee687853cfa9b238
전현준Jeonhyunjun <hyunjun050915@gmail.com>
2021-08-03 23:07:19 +0900

    Add files via upload

---
+++ 자료구조/std__array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+
+    array<int, 10>arr1;
+    
+    arr1[0] = 1;
+
+    cout << "arr1배열의 1번째 원소" << arr1[0] << endl;
+
+    array<int,4> arr2{1,2,3,4,5,6};
+
+    cout<<"arr2의 모든 원소 : " << " ";
+
+    for(int i = 0; i < arr2.size(); i++)
+        cout<<arr2[i]<<" "; 
+        cout << endl;
+
+
+    return 0;
+}

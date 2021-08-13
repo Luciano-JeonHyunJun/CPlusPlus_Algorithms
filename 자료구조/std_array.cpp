7905aa638a292f5f7bd594ba6c9f96c7b3281673
전현준Jeonhyunjun <jhj091542@gmail.com>
2021-08-13 23:41:02 +0900

    Update std_array.cpp

--- 자료구조/std_array.cpp
+++ 자료구조/std_array.cpp
@@ -2,23 +2,30 @@
 #include <array>
 using namespace std;
 
-int main()
+int main () 
 {
 
-    array<int, 10>arr1;
-    
-    arr1[0] = 1;
+    // 일반적인int 타입 배열
+    int a[10] { 1 , 2 , 3 , 4 , 5 , 6 ,  7 , 8 , 9 , 10 };
 
-    cout << "arr1배열의 1번째 원소" << arr1[0] << endl;
+    // int타입의 STL array
+    array<int, 10>b { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 };
 
-    array<int,4> arr2{1,2,3,4,5,6};
-
-    cout<<"arr2의 모든 원소 : " << " ";
-
-    for(int i = 0; i < arr2.size(); i++)
-        cout<<arr2[i]<<" "; 
-        cout << endl;
+    cout << b.data() << endl;
+    cout << *b.begin() << " " << *(b.end()) << endl;
 
 
     return 0;
 }
+
+// 핵심기능
+// array::data 
+// - 배열의 시작 번지 주소를 반환해준다.
+// array::begin() & array::end
+// - array 처음과 마지막 반복자를 반환해준다.
+// array::front & array::back() 
+// - 배열의 처음과 마지막 원소를 반환해준다.
+// array::empty() 
+// - 배열이 비어있는지 확인해준다, 비어있으면 true , 원소가 있다면 false를 반환한다.
+// array::size() & array::max_size() 
+// - 배열의 현재 원소 개수와 최대 원소 개수를 반환 해준다.

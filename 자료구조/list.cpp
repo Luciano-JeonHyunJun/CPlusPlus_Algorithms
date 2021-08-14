03bbfde70aa1bb20fff251dc4056b30ca00223a8
전현준Jeonhyunjun <jhj091542@gmail.com>
2021-08-14 22:45:59 +0900

    Update list.cpp

--- 자료구조/list.cpp
+++ 자료구조/list.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <list>
 
+using namespace std;
+
+int main()
+{
+
+    list<int> test;
+
+    test.push_front(5); // 원소 추가
+    test.emplace_front(7);// 제일 앞에 원소 추가
+
+    test.push_back(2); 
+    test.emplace_back(1); // 제일 뒤에 원소 추가
+     
+
+    for (auto& num : test)
+        cout << num << " ";
+    cout << endl;
+
+    
+
+
+    
+
+    return 0;
+}

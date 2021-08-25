7b9abbc3d4272768d7427a2c262529fed0b87faf
전현준Jeonhyunjun <jhj091542@gmail.com>
2021-08-25 23:12:32 +0900

    Update vector.cpp

--- 자료구조/vector.cpp
+++ 자료구조/vector.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 int main(){
  
-  vctor<int,10>v1 //기본선언
+  	vector<int> v; //int 타입 vector 생성
+	
+	vector<int>v = {1,2,3,4};
+
+	vector<int> v[10]; //int형 벡터 사이즈는 10으로 생성
+
+	vector<int> v[]={{1,2}, {3,4}}; //2차원 벡터 생성
+
+	vector<vector<int>>v; //2차원 벡터 생성 (행과 열 모두 가변)
+
+	
   
   
   

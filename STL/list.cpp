40b02065284673d01c7604efb23498625a8f4822
Jeonhyunjun <hyunjun050915@gmail.com>
2021-11-13 01:32:03 +0900

    Add files via upload

---
+++ STL/list.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <list>
+using namespace std;
+
+
+int main()
+{
+	
+	list<int> nums{1,2,3,4,5}; 
+	
+	nums.emplace_back(6);
+	nums.emplace_front(0);
+	
+	auto it = find(nums.begin(),nums.end(), 3); // 3이라는 element 를 찾은 후 그 위치에 100이라는 숫자를 대입 
+	if (it != nums.end()) // 만약 3 이 없는 경우를 위해 조건을 검 
+	{
+		nums.emplace(it, 100);
+	}
+	
+	
+	
+	for(auto num : nums)
+	{
+		cout << num << " ";
+	}
+	
+	
+	cout << endl;
+	
+}
+

123ecb52c27b2e67984ae1c40aa3c5a9ab71a999
Jeonhyunjun <jhj091542@gmail.com>
2021-11-14 02:07:12 +0900

    그림 그리면서 하는게 가장 나을꺼 같네 ㅋㅋ..

--- 자료구조/list.cpp
+++ 자료구조/list.cpp
@@ -4,47 +4,26 @@ using namespace std;
 
 int main()
 {
-// 기본 선언문
-
-    list<int> list1;
-    
-    list1.push_back(5);
-    list1.push_back(6);
-    list1.push_back(7);
-    list1.push_back(8);
-    list1.push_back(9);
-    list1.push_back(10);
-    
-    list1.pop_back(); // 제일 뒤 원소를 삭제
-
-    list1.push_front(4);
-    list1.push_front(3);
-    list1.push_front(2);
-    list1.push_front(1);
-    list1.push_front(0);
-
-    list1.pop_front(); // 제일 앞 원소를 삭제
- 
-    cout << " list front value: "<<list1.front() << endl;
-    cout << " list end value: "<< list1.back() << endl; 
-
-    cout << " list size: " << list1.size() << endl;
-
-    cout << list1.max_size() << endl;
-    cout << "empty: " <<list1.empty() << endl; //list1이 비었는지 안비었는지 확인해주는것
-
-    list<int>::iterator begin_iter = list1.begin(); // auto begin_iter = list1.begin() 도 가능 
-    list<int>::iterator end_iter = list1.end(); // auto end_iter = list1.end() 도 가능
-
-    begin_iter++; // 2번째를 가르키는 iterator
-    list1.insert(begin_iter,2);
-
-    end_iter--; 
-    list1.erase(end_iter);
-
-    cout << "list1 sort:" << list1.sort() << endl;
-
-    //cout << "list" << distance(list1.begin,begin_iter) + 1 << "element: " *begin_iter << endl;
-
-    return 0;
+	list<int> num; // 기본 생성 
+	//list<int> num2(10); // 기본값이 0으로 초기화된 원소 10개를 가진 리스트 생성 
+	//list<int> num3(3,2);//2값으로 초기화 된 원소3개를 가진 list를 만듬
+	//list<int> num4(num3)//num3으로 num4를 복사 
+	 
+	num.assign(3, 4); //4로 초기화된 3개의 원소를 할당해줌
+	num.front(); // 맨 앞에 원소를 반환해줌
+	num.back(); // 맨 뒤에 원소를 반환해줌 
+	num.begin(); // 맨 앞의 원소를 가리키는 iterator을 반환해준다
+	num.end(); //맨 마지막의 다음을 원소 
+	num.rend(); //뒤에서부터 순차적으로 원소를 접근할때 쓰임.end와 동일
+	num.push_back(30); //맨 마지막에 30을 삽입  
+	num.push_front(20);// 맨 앞에 20을 삽입
+	num.pop_back(); // 맨 마지막 원소를 제거
+	num.pop_front(); // 맨 앞 원소를 제거
+	num.insert(iter, k);// iter가 가리키는 위치에 k를 삽입 
+	num.erase(iter); // iterator이 가리키는 원소를 제거합니다. 
+	num.size(); // 원소의 사이즈를 반환
+	num.remove(20); // 20과 같은 원소를 모두 제거
+	   
+	
+	return 0;
 }
